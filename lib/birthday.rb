d7ce98cdd57e79866518bf7bd16ac4017e32c498
Cody Dupuis <codydupuis99@gmail.com>
2020-08-05 09:14:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,5 +8,14 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
 end
-
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age <= 12
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    else
+    end
+  end
+end
+
